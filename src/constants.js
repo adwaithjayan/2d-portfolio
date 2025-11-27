aa0305ceba387364edeb14bd043bf76eaef977b2
Adwaith <adwaithjayan124@gmail.com>
2025-11-27 19:45:13 +0530

    feat: initialize 2D portfolio project with Vite and Kaboom.js
    - Add index.html for the main application structure
    - Create package.json with scripts for development, build, and preview
    - Add map.json for the game map configuration
    - Include binary assets: map.png, monogram.ttf, and spritesheet.png
    - Implement constants.js for dialogue data and scale factor
    - Set up kaboomCtx.js for Kaboom.js context
    - Develop main.js for game logic and player interactions
    - Create utils.js for dialogue display and camera scaling functions
    - Configure Vite with a basic setup in vite.config.js

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/adwaithjayan" target="_blank">Github</a>!`,
+  'ec-degree': `This is my ECE degree. I hung it on the wall because I'm proud of it. It was a very technical degree but I think it gave me a good foundation.`,
+  'sofa-table': `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@javascriptmastery" target="_blank">JavaScript Mastery
+</a>, <a href="https://www.youtube.com/@MaxImagination" target="_blank">Max Imagination</a> and <a href="https://www.youtube.com/@notjustdev" target="_blank">notJust․dev</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/adwaithjayan target="_blank">Check it out?</a>
+  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching thistutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `There are a lot of programming books on my shelves.
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Adwaith's portfolio, just close the tab.`,
+};
